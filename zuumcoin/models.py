fa0258bfca8c77c8a9623f4e57c4756185d1fd36
linkzero177 <dohungnghia@gmail.com>
2019-04-02 15:23:07 -0400

    Coin model was modified

--- zuumcoin/models.py
+++ zuumcoin/models.py
@@ -2,14 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models import Sum
 
 # Create your models here.
-total = 0
 
 class Coin(models.Model):
     coin_type = models.CharField(max_length=100)
     coin_count = models.DecimalField(max_digits=3,decimal_places=0)
-
+    coin_value = models.DecimalField(max_digits=3,decimal_places=0)
+    total_value = models.DecimalField(max_digits=3,decimal_places=0)
+    
     def __str__(self):
         return self.coin_type
 
@@ -17,10 +19,14 @@ class Coin(models.Model):
         return self.coin_count
 
     def increase(self):
-        global total
         self.coin_count += 1
-        total += 1
+        self.total_value += coin_value
+        
+    def reset(self):
+        self.coin_count = 0
+        self.total_value = 0
+        
     @staticmethod
     def find_total():
-        global total
-        return total
+        total_dict = Coin.objects.aggregate(total=Sum('total_value'))
+        return total_dict['total']
